09ee6a6b87b75fca4018370fc76e68a9634851c2
MVNavaneeth <mvnavaneeth5@gmail.com>
2024-10-12 16:40:03 +0530

    Added gallery

--- src/components/AboutUs/AboutUs.js
+++ src/components/AboutUs/AboutUs.js
@@ -38,11 +38,20 @@ const AboutUs = () => {
                 <div className="gallery-section">
                     <h3 className="gallery-title">Our Journey in Pictures</h3>
                     <div className="gallery-collage">
-                        <img src="image1.jpg" alt="Destination 1" className="gallery-image img1" />
-                        <img src="image2.jpg" alt="Destination 2" className="gallery-image img2" />
-                        <img src="image3.jpg" alt="Destination 3" className="gallery-image img3" />
-                        <img src="image4.jpg" alt="Destination 4" className="gallery-image img4" />
-                        <img src="image5.jpg" alt="Destination 5" className="gallery-image img5" />
+                        <img src="/foxy_images/foxy1.jpg" alt="Destination 1" className="gallery-image img1" />
+                        <img src="/foxy_images/foxy2.jpg" alt="Destination 2" className="gallery-image img2" />
+                        <img src="/foxy_images/foxy3.jpg" alt="Destination 3" className="gallery-image img3" />
+                        <img src="/foxy_images/foxy4.jpg" alt="Destination 4" className="gallery-image img4" />
+                        <img src="/foxy_images/foxy5.jpg" alt="Destination 5" className="gallery-image img5" />
+                        <img src="/foxy_images/foxy6.jpg" alt="Destination 5" className="gallery-image img5" />
+                        <img src="/foxy_images/foxy7.jpg" alt="Destination 5" className="gallery-image img5" />
+                        <img src="/foxy_images/foxy8.jpg" alt="Destination 5" className="gallery-image img5" />
+                        <img src="/foxy_images/foxy9.jpg" alt="Destination 5" className="gallery-image img5" />
+                        <img src="/foxy_images/foxy10.jpg" alt="Destination 5" className="gallery-image img5" />
+                        <img src="/foxy_images/foxy11.jpg" alt="Destination 5" className="gallery-image img5" />
+                        <img src="/foxy_images/foxy12.jpg" alt="Destination 5" className="gallery-image img5" />
+                        <img src="/foxy_images/foxy13.jpg" alt="Destination 5" className="gallery-image img5" />
+                        <img src="/foxy_images/foxy14.jpg" alt="Destination 5" className="gallery-image img5" />
                     </div>
                 </div>
             </div>
